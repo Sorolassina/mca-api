782de993f788ad210756c6dfd98818b201b0d1cf
Soro lassina <sorolassina58@gmail.com>
2025-03-30 23:08:36 +0200

    fix verif qpv et siret avant validation

--- app/routes/route_qpv.py
+++ app/routes/route_qpv.py
@@ -1,12 +1,50 @@
 from fastapi import APIRouter,Request
 from app.schemas.schema_qpv import Adresse
 from app.services.service_qpv import verif_qpv
+import time
 
 router = APIRouter()
 
 # On va dans un premier temps récupérer l'adresse géographique et le valider
 @router.post("/qpv_check")
 async def get_adresse(address:Adresse, request: Request) :
+
+    start_time = time.time()  # ⏱️ début
+    data = address.model_dump()
+
     print("✅ Adresse validée au niveau du route :", address)
-    recherche = await verif_qpv(address.model_dump(), request)
+
+    # Vérifie si tous les champs sont vides
+    if all(value is None or value == '' for value in data.values()):
+        return {
+            "address": "Adresse mal formatté",
+            "nom_qp": "Aucun QPV",
+            "distance_m": "N/A",
+            "carte": "",
+            "image_url":"",
+            "image_encoded": ""
+        }
+    
+    # Exemple de vérification basique de format
+    if 'adresse' in data:
+        adr = data.get("adresse", "").strip()
+        if len(adr) < 5 or adr.isdigit():
+            return {
+            "address": "Adresse mal formatté",
+            "nom_qp": "Aucun QPV",
+            "distance_m": "N/A",
+            "carte": "",
+            "image_url":"",
+            "image_encoded": ""
+        }
+
+    # Si tout est OK, on continue la vérification QPV
+    recherche = await verif_qpv(data, request)
+
+    duration = round(time.time() - start_time, 2)
+
+    print("✅ fin d'exécution :", address)
+
+    print(f"⏱️ execution_time_sec: {duration}")
+
     return recherche
--- app/routes/route_siret_pappers.py
+++ app/routes/route_siret_pappers.py
@@ -1,11 +1,33 @@
 from fastapi import APIRouter, Request
 from app.services.service_siret_pappers import get_entreprise_process
 from app.schemas.schema_siret import SiretRequest
+import time
 
 router = APIRouter()
 
 @router.post("/siret")
 async def get_entreprise(siret_request: SiretRequest, request: Request):
+     # Exemple de vérification basique de format
+    data = siret_request.model_dump()
+    start_time = time.time()  # ⏱️ début
+    # Vérification basique du format du SIRET
+    if 'siret_request' in data:
+        sir = data.get("siret_request", "").strip()
+        if not (len(sir) == 9 or len(sir) == 14) or not sir.isdigit():
+            return {
+                "message": "SIRET incorrect",
+                "download_url": "",  
+                "csv_file": "",
+                "entreprise_data": ""
+            }
+        
     numero_siret = siret_request.numero_siret[:9]
-    return await get_entreprise_process(numero_siret, request)
+
+    infosentreprise=await get_entreprise_process(numero_siret, request)
+
+    duration = round(time.time() - start_time, 2)
+
+    print(f"⏱️ execution_time_sec: {duration}")
+
+    return infosentreprise
     
